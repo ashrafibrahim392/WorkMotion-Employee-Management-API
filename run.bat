55607b068eeb4140b2189f85a38c08e24f6da485
Ashraf Ibrahim <ashrafibrahim392@hotmail.com>
2023-02-04 13:59:33 +0200

    adding env file

--- run.bat
+++ run.bat
@@ -1,10 +1,5 @@
 docker build -t ashraf/workmostiontask .
-
-set  db_pass=password
-set  db_url=jdbc:h2:mem:mydb
-set   db_user=sa
-
-docker run -d  --name employee-management-api -p %1:8080   -e db_pass -e db_url -e db_user   ashraf/workmostiontask 
+docker run -d  --name employee-management-api -p %1:8080 --env-file=env-file    ashraf/workmostiontask
 
 
 
--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
-docker build -t ashraf/workmostiontask -f ./Dockerfile . 
-
-export  db_pass=password
-export  db_url=jdbc:h2:mem:mydb
-export   db_user=sa
-
-docker run -d  --name employee-management-api -p $1:8080   -e db_pass -e db_url -e db_user   ashraf/workmostiontask 
+docker build -t ashraf/workmostiontask -f ./Dockerfile .
+docker run -d  --name employee-management-api -p $1:8080 --env-file=env-file    ashraf/workmostiontask
 
 
 
